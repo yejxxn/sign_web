7f028403a5479f27cb9b5d2fd4195dc2b81d59dd
yejoon kim <yeijoonk@naver.com>
2025-05-21 21:13:05 +0900

    Fix endpoint name conflict in main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+from App import analyze, predict as predict_rule
+from predict_api import predict as predict_gemini
 from flask import Flask
 from flask_cors import CORS
 import os
-from App import analyze, predict as predict_rule
-from predict_api import predict as predict_gemini
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.route('/analyze', methods=['POST'])(analyze)
-app.route('/predict-rule', methods=['POST'])(predict_rule)
-app.route('/predict', methods=['POST'])(predict_gemini)
+app.route('/predict-rule', methods=['POST'], endpoint='predict_rule')(predict_rule)
+app.route('/predict', methods=['POST'], endpoint='predict_gemini')(predict_gemini)
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
